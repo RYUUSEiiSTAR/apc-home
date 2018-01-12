22ff1ef53794b291d12e96fa77a27d2e9474ae5e
RYUUSEiiSTAR <tee.nguyen+github@live.com.au>
2018-01-12 16:48:38 +1100

    Add files via upload
    hopefully this works, test it when not behind a the great firewall of ......

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
+const https = require('http');
+const querystring = require('querystring');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello World' });
+const params = querystring.stringify({
+    api_key: '',
+    method: 'flickr.photosets.getPhotos',
+    photoset_id: '72157689007399362',
+    user_id: '137676527@N05',
+    extras: 'description',
+    format: 'json',
+    jsoncallback: '1'
+});
+
+const options = {
+    hostname: 'api.flickr.com',
+    path: '/services/rest/?' + params
+}
+
+app.get('/api/flickr', (apiRequest, apiResponse) => {
+    https.get(options, (req, res) => {
+        let body;
+        console.log(body);
+        res.on('data', (data) => {
+            body += data;
+            console.log(`${data}`);
+        })
+        res.end('end', () => {
+            console.log(`${body}`);
+            body = JSON.parse(body)
+            apiResponse.send({ 'response': body });
+        })
+    }).on('error', (error) => {
+        console.log(error);
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
