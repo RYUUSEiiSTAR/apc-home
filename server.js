5c072d1cd9f0d2db91948db397b720cd61aac02f
ryuuseiistar <tee.nguyen+github@live.com.au>
2018-01-15 10:13:19 +1100

    changed allowed http methods

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const params = querystring.stringify({
 
 const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
