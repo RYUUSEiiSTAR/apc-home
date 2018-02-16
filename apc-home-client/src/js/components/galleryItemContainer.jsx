1e16e9bbf3256a3ef943b637ff8226c927230a9b
ryuuseiistar <tee.nguyen+github@live.com.au>
2018-02-16 17:14:26 +1100

    hopefully this works... will test laterrr

--- apc-home-client/src/js/components/galleryItemContainer.jsx
+++ apc-home-client/src/js/components/galleryItemContainer.jsx
@@ -13,19 +13,47 @@ export default class GalleryItemContainer extends React.Component {
     }
 
     GalleryItemContainer() {
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:3002/api/flickr-photosets',
-            success: function(data) {
-                let response = JSON.parse(data);
+        // $.ajax({
+        //     type: 'GET',
+        //     url: 'https://apc-home.herokuapp.com/api/flickr-photosets',
+        //     success: function(data) {
+        //         let response = JSON.parse(data);
+        //         let galleryItems = [];
+        //         $.each(response.photoset.photo, function(idx, photo) {
+        //             let title = photo.title;
+        //             let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+        //             let description = photo.description._content;
+        //             let galleryItem = <GalleryItem title = { title } image = { image } description = { description } />;
+        //             galleryItems.push({idx, galleryItem});
+        //         });
+
+        //         let gallery = galleryItems.map((item) =>
+        //             <li key = { item.idx }>{ item.galleryItem }</li>
+        //         );
+
+        //         this.setState((prevState, props) => ({
+        //             gallery: gallery
+        //         }));
+        //     }.bind(this),
+        //     error: function(data) {
+        //         let response = JSON.parse(data);
+        //     }
+        // });
+
+        let response;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://localhost:3002/api/flickr-photosets');
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                response = JSON.parse(xhr.responseText);
                 let galleryItems = [];
-                $.each(response.photoset.photo, function(idx, photo) {
+                for (idx = 0; idx < response.photoset.photo.length; idx++) {
                     let title = photo.title;
                     let image = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
                     let description = photo.description._content;
                     let galleryItem = <GalleryItem title = { title } image = { image } description = { description } />;
                     galleryItems.push({idx, galleryItem});
-                });
+                };
 
                 let gallery = galleryItems.map((item) =>
                     <li key = { item.idx }>{ item.galleryItem }</li>
@@ -34,11 +62,10 @@ export default class GalleryItemContainer extends React.Component {
                 this.setState((prevState, props) => ({
                     gallery: gallery
                 }));
-            }.bind(this),
-            error: function(data) {
-                let response = JSON.parse(data);
+            } else {
+                response = "Request failed. Status code: " + xhr.status;
             }
-        });
+        }
     }
 
     render() {
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3002;
 
 const params = querystring.stringify({
-    api_key: '',
+    api_key: process.env.FLICKR_KEY,
     method: 'flickr.photosets.getPhotos',
     photoset_id: '72157689007399362',
     user_id: '137676527@N05',
@@ -16,7 +16,7 @@ const params = querystring.stringify({
 });
 
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Origin', "http://airportpistolclub.com.au");
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
